8c9b4e91316661a7268e76d7a35045df95feb527
jtr13 <jtr13@columbia.edu>
2018-05-22 10:36:58 -0700

    Initial commit

---
+++ R/functions.R
@@ -0,0 +1,16 @@
+# for testing purposes only... feel free to modify
+
+f1 <- function(x) {
+  f3(3)
+  }
+
+f2 <- function(x) {}
+
+f3 <- function(x) {
+  f2(4)
+  f2(7)
+  }
+
+f4 <- function(x) {
+  f2(5)
+}
